4161b4e296c42bc706d6e77d5ff037693bf1f333
daniel-d7a <eyad.taym.saba@gmail.com>
2023-01-26 17:20:55 +0200

    added some files

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -1,14 +1,14 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-// ENV variables 
+// ENV variables
 // - AWS_ACCESS_KEY_ID
 // - AWS_SECRET_ACCESS_KEY
 // Are Also needed
 
 export const config = {
   username: `${process.env.POSTGRES_USERNAME}`,
-  password: process.env.POSTGRES_PASSWORD,
+  password: `${process.env.POSTGRES_PASSWORD}`,
   database: process.env.POSTGRES_DB,
   host: process.env.POSTGRES_HOST,
   aws_region: process.env.AWS_REGION,
@@ -19,3 +19,5 @@ export const config = {
     secret: process.env.JWT_SECRET,
   },
 };
+
+console.log(config);
