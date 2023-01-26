d40406f0108891d2e9eb672958c8cf51b1784496
daniel-d7a <eyad.taym.saba@gmail.com>
2023-01-26 19:04:59 +0200

    different bucket

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www s3://eyad-udagram-bucket
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://eyad-udagram-bucket
+aws s3 cp --recursive --acl public-read ./www s3://elasticbeanstalk-us-east-1-344114408757
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://elasticbeanstalk-us-east-1-344114408757
