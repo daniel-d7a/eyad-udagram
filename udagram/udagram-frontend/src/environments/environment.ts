d588d6cec6f81889420315c5a26f613217b27a1c
daniel-d7a <eyad.taym.saba@gmail.com>
2023-02-21 00:21:26 +0200

    fixed api link

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: "Udagram",
-  apiHost: "http://udagram-app-eyad-dev.us-east-1.elasticbeanstalk.com/api/v0/",
+  apiHost: "http://udagram-app-eyad-dev.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
